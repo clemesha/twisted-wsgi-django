382d5900890bd7eb609b67655d8bb283acb95b46
David Lechón Quiñones <dalequi@gmail.com>
2013-11-25 15:05:27 +0100

    Fixed static path

--- mydjangosite/settings.py
+++ mydjangosite/settings.py
@@ -90,7 +90,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/david/Desktop/twisted-wsgi-django/mydjangosite/static/",
+    "./static/",
 )
 
 INSTALLED_APPS = (
